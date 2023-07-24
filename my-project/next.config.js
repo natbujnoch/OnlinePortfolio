6cf839435c309047bef4764f1d8a9aeba68042f0
natbujnoch <natbujnoch@gmail.com>
2023-07-24 17:15:29 -0500

    added maintenance page

--- my-project/next.config.js
+++ my-project/next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+  
+module.exports = {
+    
+    async redirects() {
+      return [
+        //triggers maintenance mode
+        {
+          source: "/((?!maintenance).*)",
+          destination: "/maintenance.html",
+          permanent: false, 
+        },
+        //triggers normal mode
+        // {
+        //     source: "/maintenance.html",
+        //     destination: "/",
+        //     permanent: false,
+        //   },
+      ];
+    },
+  };
